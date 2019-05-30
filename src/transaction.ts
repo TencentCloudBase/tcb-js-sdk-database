3716e9e4316484deb508ef287a2d11c67e8d2370
dongyuanxin <yuanxin.me@gmail.com>
2019-05-30 17:14:15 +0800

    add: Transaction类, startTransaction 和 runTransaction方法

---
+++ src/transaction.ts
@@ -0,0 +1,77 @@
+// import { DocumentReference } from './document'
+import { Db } from './db'
+
+export class Transaction {
+
+  private _id: string;
+
+  private _db: Db;
+
+  private _request: any;
+
+  public constructor(db: Db) {
+    this._db = db
+    this._request = new Db.reqClass(this._db.config)
+  }
+
+  async init(): Promise<void> {
+    const { data } = await this._request.send('database.startTransaction')
+    this._id = data.TransactionId
+  }
+
+  // async get(documentRef: DocumentReference): Promise<DocumentSnapshot> {
+  //   const param = {
+  //     documentRef,
+  //     TransactionId: this._id
+  //   }
+  //   const res = await this._request.send('database.getInTransaction', param)
+  //   return res
+  // }
+
+  async commit(): Promise<CommitResult> {
+    const param = {
+      transactionId: this._id
+    }
+    const res: CommitResult = await this._request.send('database.commitTransaction', param)
+    return res
+  }
+
+  // async rollback(): Promise<any> {
+  //   const param = {
+  //     transactionId: this._id
+  //   }
+  //   const res = await this._request.send('database.abortTransaction', param)
+  //   return res
+  // }
+}
+
+export async function startTransaction(): Promise<Transaction> {
+  const transaction = new Transaction(this)
+  await transaction.init()
+  return transaction
+}
+
+export async function runTransaction(
+  callback: (transaction: Transaction) => void | Promise<any>,
+  times: number = 3
+): Promise<void> {
+  if (times <= 0) {
+    return
+  }
+  try {
+    const transaction = new Transaction(this)
+    await callback(transaction)
+    await transaction.commit()
+  } catch (error) {
+    arguments.callee(callback, --times)
+  }
+}
+
+// interface DocumentSnapshot {
+//   requestId: string,
+// }
+
+interface CommitResult {
+  requestId: string,
+  data: object
+}
